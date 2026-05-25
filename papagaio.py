5c56506c7a221008be01c9bdc0a3aae9d31dcd0b
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-05-25 16:01:20 -0300

    papagaio poliglota em py

---
+++ papagaio.py
@@ -0,0 +1,11 @@
+while True:
+    perna = input()
+    if perna == 'esquerda':
+        print('ingles')
+    elif perna == 'direita':
+        print('frances')
+    elif perna == 'nenhuma':
+        print('portugues')
+    else:
+        print('caiu')
+        break
